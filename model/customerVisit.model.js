fa10fec6e9cd684ad84ec918198f84a7595ef0df
TanyaSoni2912 <tanu139sona@gmail.com>
2024-09-02 16:15:44 +0530

    Order should be a array

--- model/customerVisit.model.js
+++ model/customerVisit.model.js
@@ -1,38 +1,43 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
-const customerVisitSchema = new Schema({
+const customerVisitSchema = new Schema(
+  {
     customer_id: {
-        type: Schema.Types.ObjectId,
-        ref: 'Customer'
+      type: Schema.Types.ObjectId,
+      ref: 'Customer',
     },
     // subscription_plan_id: {
     //     type: Schema.Types.ObjectId,
     //     ref: 'SubscriptionPlan'
     // },
-    order_id: {
+    order_id: [
+      {
         type: Schema.Types.ObjectId,
-        ref: 'Order'
-    },
+        ref: 'Order',
+      },
+    ],
     location_id: {
-        type: Schema.Types.ObjectId,
-        ref: 'Location',
-        required: [true, "Location Id is required"]
+      type: Schema.Types.ObjectId,
+      ref: 'Location',
+      required: [true, 'Location Id is required'],
     },
     start_time: {
-        type: Date,
-        default: Date.now(),
-        required: [true, "Start Time is required"]
+      type: Date,
+      default: Date.now(),
+      required: [true, 'Start Time is required'],
     },
     end_time: {
-        type: Date,
-    }
-}, {
+      type: Date,
+    },
+  },
+  {
     timestamps: {
-        createdAt: 'created_at',
-        updatedAt: 'updated_at',
-    }
-});
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    },
+  },
+);
 
 const CustomerVisit = mongoose.model('CustomerVisit', customerVisitSchema);
 module.exports = CustomerVisit;
