74cf19cd00a4c8e224e586bb56859b6b23ce757e
TanyaSoni2912 <tanu139sona@gmail.com>
2024-09-17 15:14:08 +0530

    Time stamps in Service Modal Added

--- model/service.model.js
+++ model/service.model.js
@@ -1,20 +1,23 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const serviceSchema = new Schema({
-  serviceName: {
-    type: String,
-    required: [true, 'Service Name is required'],
+const serviceSchema = new Schema(
+  {
+    serviceName: {
+      type: String,
+      required: [true, 'Service Name is required'],
+    },
+    minutesAvailable: {
+      type: Number,
+      required: [true, 'Minutes is required'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Price is required'],
+    },
   },
-  minutesAvailable: {
-    type: Number,
-    required: [true, 'Minutes is required'],
-  },
-  price: {
-    type: Number,
-    required: [true, 'Price is required'],
-  },
-});
+  { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } },
+);
 
 serviceSchema.pre('updateOne', function (next) {
   this.set({ updatedAt: Date.now() });
