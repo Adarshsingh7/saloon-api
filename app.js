fb8fb909a08af67367c451da786c9e22bc919372
TanyaSoni2912 <tanu139sona@gmail.com>
2024-09-23 12:43:07 +0530

    vercel hosted fronted url pasted

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://salon-dash-board.vercel.app',
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
