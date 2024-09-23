74708a92d80d1bc210b719c1a3a85d59575c1c08
TanyaSoni2912 <tanu139sona@gmail.com>
2024-09-23 11:51:05 +0530

    Frontend url changed

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: 'http://localhost:3001',
+  origin: 'http://localhost:3000',
   optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
